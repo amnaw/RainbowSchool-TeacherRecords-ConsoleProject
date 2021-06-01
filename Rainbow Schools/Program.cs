0d26b66390ee463044b3645df1b99652fff09d64
Amna Alwzrah <aminah.l.1@windowslive.com>
2021-06-01 07:30:02 +0300

    First commit

---
+++ Rainbow Schools/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+
+namespace Rainbow_Schools
+{
+    class Program
+    {
+        static readonly string StudentsDataFile = "C:\\Users\\amina\\Desktop\\.NET developer Course\\Rainbow Schools\\Rainbow Schools Students.txt";
+        static void Main(string[] args)
+        {
+            if (File.Exists(StudentsDataFile))
+            {
+                // Read entire text file content in one string    
+                string text = File.ReadAllText(StudentsDataFile);
+                Console.WriteLine(text);
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
